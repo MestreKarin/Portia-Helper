d8d50a170a3d962de53fd210eb58ec49ea10f8cd
Mestre Karin <brenoapereira@gmail.com>
2019-12-11 13:14:39 -0300

    Create PlayerPatcher.cs

---
+++ Core/Patchers/PlayerPatcher.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using Pathea;
+using PortiaHelper.Core.Loaders;
+
+namespace PortiaHelper.Core.Patchers
+{
+	[HarmonyPatch(typeof(Player))]
+	[HarmonyPatch("OnLoad")]
+	public static class PlayerPatcher
+	{
+		[HarmonyPostfix]
+		public static void Postfix() {
+			PlayerAddonsLoader.Load();
+		}
+	}
+}
