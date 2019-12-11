dd0d20d68fb72a47825947576ae578610f4f4077
Mestre Karin <brenoapereira@gmail.com>
2019-12-11 13:15:16 -0300

    Create InfiniteVp.cs

---
+++ Modules/InfiniteVp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Pathea;
+using Pathea.ActorNs;
+using Pathea.ModuleNs;
+
+namespace PortiaHelper.Modules
+{
+	public class InfiniteVp : MonoBehaviour
+	{
+		private Player _player;
+
+		public void Start() {
+			_player = Module<Player>.Self;
+		}
+
+		public void Update() {
+			if (_player.actor != null) {				
+				_player.actor.Vp = 100.0f;
+
+				if (_player.actor.CurRidableActor != null) {
+					_player.actor.CurRidableActor.Vp = 100.0f;
+				}
+			}
+		}
+	}
+}
