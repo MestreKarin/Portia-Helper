0afaf9ee3b1099fce648f310e3b2d56a4b9d7698
Mestre Karin <brenoapereira@gmail.com>
2019-12-15 21:35:00 -0300

    Create JetPackPatcher.cs

---
+++ Core/Patchers/JetPackPatcher.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using Pathea.EquipmentNs;
+
+namespace PortiaHelper.Core.Patchers
+{
+	[HarmonyPatch(typeof(JetPack))]
+	[HarmonyPatch("OnEnable")]
+	public static class JetPackPatcher
+	{
+		[HarmonyPostfix]
+		public static void Postfix(ref float ___durationValue) {
+			___durationValue = 999_999.0f;
+		}
+	}
+}
