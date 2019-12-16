a74c606f08d38bb621c11a00a0546f3142d3ef0c
Mestre Karin <brenoapereira@gmail.com>
2019-12-15 21:34:52 -0300

    Create PlayerUICtrPatcher.cs

---
+++ Core/Patchers/PlayerUICtrPatcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Harmony;
+using Pathea.UISystemNs.PlayUI;
+using PortiaHelper.Modules;
+
+namespace PortiaHelper.Core.Patchers
+{
+	[HarmonyPatch(typeof(PlayUICtr))]
+	[HarmonyPatch("OnEnable")]
+	static class PlayerUICtrPatcher
+	{
+		[HarmonyPostfix]
+		static void Postfix() {
+			var uiCtr = GameObject.FindObjectOfType<PlayUICtr>();
+
+			if (uiCtr.gameObject.GetComponent<GuiActivator>() is null) {
+				uiCtr.gameObject.AddComponent<GuiActivator>();
+			}
+		}
+	}
+}
