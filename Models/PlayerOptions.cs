0cde050068275bdae3f58d33740768efe58f4638
Mestre Karin <brenoapereira@gmail.com>
2019-12-15 21:35:11 -0300

    Create PlayerOptions.cs

---
+++ Models/PlayerOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace PortiaHelper.Models
+{
+	public class PlayerOptions
+	{
+		public float ExpRatio { get; set; }
+
+		public float GoldRatio { get; set; }
+
+		public bool GodMode { get; set; }
+
+		public bool InfiniteStamina { get; set; }
+
+		public bool InfiniteVP { get; set; }
+
+		public bool NoDrop { get; set; }
+	}
+}
