d55e5a1f4091c00ca97903447c2d126ce0e193e1
Mestre Karin <brenoapereira@gmail.com>
2019-12-15 21:31:06 -0300

    Create RectExtensions.cs

---
+++ Core/Extensions/RectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace PortiaHelper.Core.Extensions
+{
+	public static class RectExtensions
+	{
+		public static Rect Normalize(this Rect input, float textureSize) {
+			float b = 1f / textureSize;
+
+			return new Rect(b * input.x, b * input.y, b * input.width, b * input.height);
+		}
+	}
+}
