9d66ca1d7c6b324ab6d25450e8adda876fb05f26
Mestre Karin <brenoapereira@gmail.com>
2019-12-15 21:35:16 -0300

    Create GuiActivator.cs

---
+++ Modules/GuiActivator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using PortiaHelper.Gui;
+
+namespace PortiaHelper.Modules
+{
+	class GuiActivator : MonoBehaviour
+	{
+		protected ItemSpawnerGui _itemSpawner;
+
+		protected PlayerOptionsGui _playerOptions;
+
+		void Start() {
+			_itemSpawner = gameObject.AddComponent<ItemSpawnerGui>();
+			_playerOptions = gameObject.AddComponent<PlayerOptionsGui>();
+
+			_itemSpawner.enabled = false;
+			_playerOptions.enabled = false;
+		}
+
+		void Update() {
+			if (Input.GetKeyUp(KeyCode.Insert) && !_playerOptions.enabled) {
+				_itemSpawner.enabled = !_itemSpawner.enabled;
+			}
+
+			if (Input.GetKeyUp(KeyCode.Home) && !_itemSpawner.enabled) {
+				_playerOptions.enabled = !_playerOptions.enabled;
+			}
+		}
+	}
+}
