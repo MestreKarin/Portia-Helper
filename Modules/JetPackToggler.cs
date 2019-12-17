e845e6c60f83de61844c0a39ee1ac5f53194ea31
Mestre Karin <brenoapereira@gmail.com>
2019-12-16 22:33:31 -0300

    Create JetPackToggler.cs

---
+++ Modules/JetPackToggler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Pathea;
+using Pathea.ModuleNs;
+
+namespace PortiaHelper.Modules
+{
+	class JetPackToggler : MonoBehaviour
+	{
+		void Update() {
+			if (Input.GetKeyUp(KeyCode.Keypad8)) {
+				Module<Player>.Self.ToggleJetPack();
+			}
+		}
+	}
+}
